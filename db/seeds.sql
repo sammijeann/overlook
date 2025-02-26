ba7e40fdeeea7dbea35cb17bb2077e4e4eec3db7
Sammi Teel <sammijteel@gmail.com>
2025-02-26 13:09:50 -0600

    Added Tables and Seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,28 @@
--- INSERT INTO favorite_books (book_name, section, in_stock, quantity)
--- VALUES ('The Great Gatsby', 1, true, 11),
---        ('Huckleberry Finn', 1, true, 4),
---        ('100 Years of Solitude', 3, false, 0),
---        ('Things Fall Apart', 4 , false, 0 ),
---        ('Crime and Punishment', 1, true, 15),
---        ('Moby Dick', 2, true, 11),
---        ('Decameron', 5, false, 0),
---        ('The Wind and the Willows', 2, true, 11),
---        ('Ulysses', 3, true, 8),
---        ('Madame Bovary', 4 , true, 9 ),
---        ('Catcher in the Rye', 1, true, 23),
---        ('Catch-12', 5, true, 18),
---        ('Hamlet', 5, true, 14);
+INSERT INTO department (name)
+VALUES  ('Art'),
+        ('Design'),
+        ('Tech'),
+        ('Production');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Art Director', 000, 1),
+        ('3D Artist', 000, 1),
+        ('2D Artist', 000, 1),
+        ('Lead Designer', 000, 2),
+        ('Level Designer', 000, 2),
+        ('Gameplay Designer', 000, 2),
+        ('Lead Programmer', 000, 3),
+        ('Gameplay Programmer', 000, 3),
+        ('Technical Artist', 000, 3),
+        ('Producer', 000, 4),
+        ('Scrum Master', 000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Mae', 'Satterfield', 10, NULL),
+        ('Sammi', 'Teel', 9, 1),
+        ('Nina', 'Vidal', 1, 1),
+        ('Sierra', 'Jacobs', 7, 2),
+        ('Jackson', 'McLellan', 4, 1),
+        ('Marlena', 'Star', 2, 3),
+        ('Haoran', 'Zhang', 3, 3);
        
